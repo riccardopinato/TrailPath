d2aa4af805ea65916fb0a452a5a524333f5707c8
riccardopinato <riccardo121296@gmail.com>
2026-09-22 14:30:04 +0200

    fix: remove route duration interpolation lint

--- lib/features/planner/presentation/planner_screen.dart
+++ lib/features/planner/presentation/planner_screen.dart
@@ -708,7 +708,8 @@ String _formatDuration(Duration duration) {
   if (hours == 0) {
     return '${minutes} min';
   }
-  return '$hours h ${minutes.toString().padLeft(2, '0')}';
+  final minuteText = minutes.toString().padLeft(2, '0');
+  return '$hours h $minuteText';
 }
 
 class _Metric extends StatelessWidget {
