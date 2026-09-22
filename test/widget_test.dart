c950e2211c691d37f67f96c31aad27fde550f525
riccardopinato <riccardo121296@gmail.com>
2026-09-22 13:41:19 +0200

    test: disable native map platform view in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:trail_path/app/app.dart';
+import 'package:trail_path/app/app.dart';\nimport 'package:trail_path/features/planner/presentation/planner_screen.dart';
 
 void main() {
   testWidgets('TrailPath foundation boots into planner shell', (tester) async {
-    await tester.pumpWidget(const ProviderScope(child: TrailPathApp()));
+    await tester.pumpWidget(\n      ProviderScope(\n        overrides: [mapRenderingEnabledProvider.overrideWithValue(false)],\n        child: const TrailPathApp(),\n      ),\n    );
     await tester.pumpAndSettle();
 
     expect(find.text('TrailPath'), findsOneWidget);
