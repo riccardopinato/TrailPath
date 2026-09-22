cedf00c9f510bc64706b8db8ab7074165e3512ad
riccardopinato <riccardo121296@gmail.com>
2026-09-22 17:04:39 +0200

    test: expect v0.7 navigation baseline

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -22,6 +22,6 @@ void main() {
 
     expect(find.text('TrailPath'), findsOneWidget);
     expect(find.text('0 m'), findsOneWidget);
-    expect(find.text('v0.6.0'), findsOneWidget);
+    expect(find.text('v0.7.0'), findsOneWidget);
   });
 }
