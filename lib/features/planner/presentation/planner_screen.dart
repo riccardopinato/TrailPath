0ff63f5cdd0c69c7c4dc68aa0de26758f15f3b01
riccardopinato <riccardo121296@gmail.com>
2026-09-22 14:27:37 +0200

    fix: satisfy duration interpolation lint

--- lib/features/planner/presentation/planner_screen.dart
+++ lib/features/planner/presentation/planner_screen.dart
@@ -708,7 +708,7 @@ String _formatDuration(Duration duration) {
   if (hours == 0) {
     return '${minutes} min';
   }
-  return '${hours}h ${minutes.toString().padLeft(2, '0')}';
+  return '$hours h ${minutes.toString().padLeft(2, '0')}';
 }
 
 class _Metric extends StatelessWidget {
