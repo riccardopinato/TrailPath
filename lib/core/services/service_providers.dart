46fb5aa21e301d489f0763558fb7f33d597d925d
riccardopinato <riccardo121296@gmail.com>
2026-09-22 14:06:32 +0200

    feat: register live location engine

--- lib/core/services/service_providers.dart
+++ lib/core/services/service_providers.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:trail_path/core/services/service_contracts.dart';
+import 'package:trail_path/infrastructure/location/geolocator_location_engine.dart';
 import 'package:trail_path/infrastructure/maps/maplibre_map_engine.dart';
 
 final mapEngineProvider = Provider<MapEngine>(
   (ref) => const MapLibreMapEngine(),
+);
+
+final locationEngineProvider = Provider<LocationEngine>(
+  (ref) => const GeolocatorLocationEngine(),
 );
