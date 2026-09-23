510e947b4ebdec3de04f41ed912abe87d74889b0
riccardopinato <riccardo121296@gmail.com>
2026-09-23 16:22:44 +0200

    v0.9.6: remove redundant typed data import

--- lib/features/planner/presentation/planner_screen.dart
+++ lib/features/planner/presentation/planner_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math' as math;
-import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
