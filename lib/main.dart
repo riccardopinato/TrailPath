9f7004acafc767a8f1169582b50e608b4ef40208
riccardopinato <riccardo121296@gmail.com>
2026-09-23 16:15:37 +0200

    v0.9.6: prewarm MapLibre engine

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
 import 'package:trail_path/app/app.dart';
 import 'package:trail_path/core/logging/app_logger.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  unawaited(MapLibreMap.preWarm());
   AppLogger.info('TrailPath bootstrap');
   runApp(const ProviderScope(child: TrailPathApp()));
 }
