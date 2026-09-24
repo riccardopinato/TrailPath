d19d719cee35e6fcaa186385898811fd3a6e678c
riccardopinato <riccardo121296@gmail.com>
2026-09-24 10:24:52 +0200

    TrailPath v0.9.9: align network identity

--- lib/core/config/map_config.dart
+++ lib/core/config/map_config.dart
@@ -1,5 +1,5 @@
 abstract final class MapConfig {
-  static const String appVersion = '0.9.8.1';
+  static const String appVersion = '0.9.9';
   static const String projectUrl =
       'https://github.com/riccardopinato/TrailPath';
 
