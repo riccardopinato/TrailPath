3ecd19f7baa76c48cab0f0186fa18acc6597f08d
riccardopinato <riccardo121296@gmail.com>
2026-09-24 11:25:27 +0200

    TrailPath v0.10.0: sync lib/core/config/map_config.dart

--- lib/core/config/map_config.dart
+++ lib/core/config/map_config.dart
@@ -1,5 +1,5 @@
 abstract final class MapConfig {
-  static const String appVersion = '0.9.9';
+  static const String appVersion = '0.10.0';
   static const String projectUrl =
       'https://github.com/riccardopinato/TrailPath';
 
