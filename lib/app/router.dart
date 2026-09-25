9f22896f4958c2fc9f08417eb83c4a3987aaa11b
riccardopinato <riccardo121296@gmail.com>
2026-09-25 14:33:17 +0200

    feat: route first launch through onboarding

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:trail_path/features/home/presentation/home_shell.dart';
+import 'package:trail_path/features/onboarding/presentation/onboarding_gate.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
   final router = GoRouter(
     initialLocation: '/',
     routes: [
-      GoRoute(path: '/', builder: (context, state) => const HomeShell()),
+      GoRoute(path: '/', builder: (context, state) => const OnboardingGate()),
     ],
   );
 
