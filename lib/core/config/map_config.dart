70b1b6105fbf41e6b3ae2d51822908895fddbdf5
riccardopinato <riccardo121296@gmail.com>
2026-09-25 10:46:22 +0200

    chore: expose v0.9.14 app version

--- lib/core/config/map_config.dart
+++ lib/core/config/map_config.dart
@@ -1,5 +1,5 @@
 abstract final class MapConfig {
-  static const String appVersion = '0.9.13';
+  static const String appVersion = '0.9.14';
   static const String projectUrl =
       'https://github.com/riccardopinato/TrailPath';
 
